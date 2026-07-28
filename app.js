c94ec627f5e90e630222bca30633721f878341f5
xiaowu-605 <2324462783@qq.com>
2026-07-28 15:17:21 +0800

    feat: 新增全局错误处理中间件

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getDb, saveDb } from './utlis/index.js'
+import { getDb, saveDb } from './utils/index.js'
 
 const app = express()
 // 中间件，处理接收到的body
@@ -15,60 +15,50 @@ app.use((req, res, next) => {
 })
 
 app.get('/', async (req, res) => {
-  try {
-    const dbjson = await getDb()
-    res.success(dbjson.users)
-  } catch (e) {
-    console.error(e) // 服务端自己看完整错误
-    // res.status(500).json({ error: e.message })
-    res.fail(e.message, 500)
-  }
+  const dbjson = await getDb()
+  res.success(dbjson.users)
 })
 
 app.post('/', async (req, res) => {
   if (!req.body || Object.keys(req.body).length === 0) {
     return res.fail('缺少用户信息')
   }
-  try {
-    // 获取到文件内容，把新增的添加进去
-    const userInfo = await getDb()
-    // 更新users
-    const lastUser = userInfo.users[userInfo.users.length - 1]
-    const id = lastUser ? lastUser.id + 1 : 1
-    userInfo.users.push({ ...req.body.users, id })
-    await saveDb(userInfo)
-    res.success(null, 'success')
-  } catch (e) {
-    res.fail(e.message, 500)
-  }
+  // 获取到文件内容，把新增的添加进去
+  const userInfo = await getDb()
+  // 更新users
+  const lastUser = userInfo.users[userInfo.users.length - 1]
+  const id = lastUser ? lastUser.id + 1 : 1
+  userInfo.users.push({ ...req.body, id })
+  await saveDb(userInfo)
+  res.success(null, 'success')
 })
 
 // 修改信息
 app.put('/:id', async (req, res) => {
-  if (!req.params) {
+  if (!req.params?.id) {
     res.fail('缺少用户id')
     return
   }
-  try {
-    const dbjson = await getDb()
-    const users = dbjson.users
-    const index = users.findIndex((item) => item.id == req.params.id)
-    if (index != -1) {
-      users[index] = {
-        ...users[index],
-        ...req.body,
-      }
-      await saveDb(dbjson)
-      res.success('修改成功')
-    } else {
-      res.fail('修改失败，当前id不存在')
+  const dbjson = await getDb()
+  const users = dbjson.users
+  const index = users.findIndex((item) => item.id == req.params.id)
+  if (index != -1) {
+    users[index] = {
+      ...users[index],
+      ...req.body,
     }
-  } catch (e) {
-    console.log('修改用户信息错误：', e)
-    res.fail(e.message, 500)
+    await saveDb(dbjson)
+    res.success('修改成功')
+  } else {
+    res.fail('修改失败，当前id不存在')
   }
 })
-
-app.listen(3000, () => {
-  console.log('Run http://localhost:3000')
+// 全局错误处理中间件, 不要每个路由都要try catch捕获错误
+app.use((err, req, res, next) => {
+  console.log(err)
+  res.fail(err.message || '服务器内部错误', 500)
+})
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Run http://localhost:${PORT}`)
 })
