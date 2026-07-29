ebf1618f59530a819e9de7835139a5e311afef7b
xiaowu-605 <2324462783@qq.com>
2026-07-29 15:21:18 +0800

    feat: mongodb数据库连接增删改查操作练习

---
+++ test.js
@@ -0,0 +1,34 @@
+import { MongoClient } from 'mongodb'
+const client = new MongoClient('mongodb://127.0.0.1:27017')
+
+async function clientFn(c) {
+  await client.connect()
+  const db = client.db('test')
+  return db.collection(c)
+}
+
+const main = async () => {
+  const user = await clientFn('user')
+  // const d = await user.find()
+  // console.log('d---', await d.toArray()) // 返回的游标，需要await   toArray
+  // const d = await user.findOne({ age: { $gt: 30 } })
+  // console.log('d--1', d)
+  // const i = await user.insertOne({ name: 'monika', age: 32 })
+  // console.log('i--', i)
+  // const is = await user.insertMany([
+  //   { name: 'monika1', age: 34 },
+  //   { name: 'kaka', age: 29 },
+  // ])
+  // console.log('is--', is)
+
+  // 更新
+  // const u = await user.updateOne({ name: 'kaka' }, { $set: { age: 60 } })
+  // console.log('u---', u)
+  // const us = await user.updateMany({ age: { $gt: 30 } }, { $set: { age: 18 } })
+
+  // 删除
+  // const d = await user.deleteOne({ name: 'kaka' })
+  // console.log('d--', d)
+  // const ds = await user.deleteMany({ age: { $lt: 20 } })
+}
+main().finally(() => client.close())
