44a7d41afd23c024305571578315199e992fc33f
xiaowu-605 <2324462783@qq.com>
2026-07-30 11:23:45 +0800

    feat: 注册验证是否已存在

--- middleware/validator/userValidator.js
+++ middleware/validator/userValidator.js
@@ -1,4 +1,5 @@
 import { body } from 'express-validator'
+import { User } from '../../model/index.js'
 
 export const registerValidator = [
   body('username').notEmpty().withMessage('用户名不能为空'),
@@ -7,12 +8,24 @@ export const registerValidator = [
     .withMessage('邮箱不能为空')
     .bail() // bail验证通过才验证后面的
     .isEmail()
-    .withMessage('邮箱格式不正确'),
+    .withMessage('邮箱格式不正确')
+    .bail()
+    .custom(async (value) => {
+      const exists = await User.findOne({ email: value })
+      if (exists) throw new Error('邮箱已存在')
+      return true
+    }),
   body('password').notEmpty().withMessage('密码不能为空'),
   body('phone')
     .notEmpty()
     .withMessage('手机号不能为空')
     .bail()
     .isMobilePhone('zh-CN')
-    .withMessage('手机号格式不正确'),
+    .withMessage('手机号格式不正确')
+    .bail()
+    .custom(async (value) => {
+      const exists = await User.findOne({ phone: value })
+      if (exists) throw new Error('手机号已被注册')
+      return true
+    }),
 ]
