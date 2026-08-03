02db499f1eed05f47e036261664b65ad50400de1
xiaowu-605 <2324462783@qq.com>
2026-08-03 15:29:51 +0800

    fix: jwt验证区分是否需要验证

--- utils/jwt.js
+++ utils/jwt.js
@@ -7,15 +7,22 @@ export const getToken = (userInfo) => {
 }
 
 // 验证token中间件
-export const requireAuth = (req, res, next) => {
-  const token = req.headers.authorization?.replace('Bearer ', '')
-  if (!token) return res.fail('请先登录', 401)
-  // 验证
-  try {
-    const userInfo = jwt.verify(token, process.env.JWT_SECRET)
-    req.user = userInfo // 把用户信息挂在 req 上，后面的路由直接用
-    next()
-  } catch (e) {
-    res.fail('Token 无效或已过期', 401)
+export const requireAuth = (require = true) => {
+  return async (req, res, next) => {
+    const token = req.headers.authorization?.replace('Bearer ', '')
+    if (token) {
+      // 验证
+      try {
+        const userInfo = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = userInfo // 把用户信息挂在 req 上，后面的路由直接用
+        next()
+      } catch (e) {
+        res.fail('Token 无效或已过期', 401)
+      }
+    } else if (require) {
+      if (!token) return res.fail('请先登录', 401)
+    } else {
+      next()
+    }
   }
 }
